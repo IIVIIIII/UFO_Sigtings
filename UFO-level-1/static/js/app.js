5574d9e54278ecec1ba8ff76a0592c048c4fd66e
IIVIIIII <kelwalls@gmail.com>
2021-04-09 17:15:02 -0700

    got it :)

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,32 +2,29 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var filterDate = d3.select("#datetime");
 
+data.forEach(function(sight) {
+  var row = tbody.append("tr");
+  Object.entries(sight).forEach(function([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
 filterDate.on("input", function() {
+
+  tbody.selectAll("tr").remove();
 
   var selDate = d3.event.target.value;
   var sights = data.filter(s => {return s.datetime == selDate});
 
-
   sights.forEach(function(sight) {
     var row = tbody.append("tr");
+    rows ++
     Object.entries(sight).forEach(function([key, value]) {
-      console.log(sight)
       var cell = row.append("td");
       cell.text(value);
+      
     });
   });
-
 });
-
-
-
-// data.forEach(function(sight) {
-//   var row = tbody.append("tr");
-//   Object.entries(sight).forEach(function([key, value]) {
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
-
-
 
