8671be7fca39c195515281ed098f2807f1219586
IIVIIIII <kelwalls@gmail.com>
2021-04-09 17:34:20 -0700

    multiple filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,7 +3,9 @@ var tbody = d3.select("tbody");
 var filterDate = d3.select("#datetime");
 var filterCity = d3.select("#city");
 
+console.log(data)
 var selDate = ""
+var selCity = ""
 
 data.forEach(function(sight) {
   var row = tbody.append("tr");
@@ -13,11 +15,15 @@ data.forEach(function(sight) {
   });
 });
 
+
+
+
 filterDate.on("input", function() {
 
   tbody.selectAll("tr").remove();
 
   selDate = d3.event.target.value;
+  console.log(selDate)
   var sights = data.filter(s => {
     if (selDate != "") {
       return s.datetime == selDate
@@ -36,3 +42,29 @@ filterDate.on("input", function() {
   });
 });
 
+
+
+
+
+filterCity.on("input", function() {
+
+  tbody.selectAll("tr").remove();
+
+  selCity = d3.event.target.value;
+  var sights = data.filter(s => {
+    if (selCity != "") {
+      return s.city == selCity
+    } else {
+      return data
+    }
+  });
+  
+  sights.forEach(function(sight) {
+    var row = tbody.append("tr");
+    Object.entries(sight).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+      
+    });
+  });
+});
